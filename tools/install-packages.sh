57403412ab15041a89def96123780afab310e831
Phil Huang <phil.huang@microsoft.com>
2025-06-02 02:11:53 +0800

    e12
    Signed-off-by: Phil Huang <phil.huang@microsoft.com>

---
+++ tools/install-packages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt install -y \
+  iputils-ping \
+  iputils-tracepath \
+  mtr \
+  git \
+  vim \
+  dnsutils \
+  python3-pip \
+  ca-certif icates \
+  curl \
+  rsync \
+  hping3
